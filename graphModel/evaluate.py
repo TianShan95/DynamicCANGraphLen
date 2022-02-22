e8b9375cee6452a3bbe2612e32eb0fe5b00c03e5
Shan Tian <ts_951117aaron@163.com>
2022-02-22 13:50:11 +0800

    debug

--- graphModel/evaluate.py
+++ graphModel/evaluate.py
@@ -57,11 +57,11 @@ def evaluate(dataset, model, args, device='cpu'):
 
         ypred_np = ypred.cpu().detach().numpy()
         print(f'ypred: {ypred_np}')
-        print(f'graph_label: {labels[batch_idx].astype(int)}')
+        print(f'graph_label: {labels[batch_idx].astype(int)[0]}')
         print(f'pred_result: {pre_label.item()}')
 
         # 制定 reward
-        if pre_label == labels[batch_idx].astype(int):
+        if pre_label == labels[batch_idx].astype(int)[0]:
             print('预测正确')
             reward = abs(ypred_np[0, 0] - ypred[0, 1])
         else:
