7f28f2a7e5787fd2412532c7678a09198669bacf
Shan Tian <ts_951117aaron@163.com>
2022-02-23 13:51:10 +0800

    debug

--- main.py
+++ main.py
@@ -59,114 +59,116 @@ def main():
     with open(log_out_file, 'w+') as f:
         f.write(f'{prog_args}\n')
 
-    # if prog_args.mode == 'test':
-    #
-    #     # 载入 模型
-    #     agent.load()
-    #     # 记录 图模型 执行 步数
-    #     graph_step = 0
-    #     # for i in range(prog_args.train_epoch):
-    #     # 随机获取 初始状态
-    #     state, _, _, _ = graph_task.benchmark_task_val(prog_args.feat, pred_hidden_dims, graph_len_, log_out_file)
-    #
-    #     while True:
-    #         action = agent.select_action(state)  # 从 现在的 状态 得到一个动作 维度是 报文长度可选择数量
-    #         # 图操作 步数 自增 1
-    #         graph_step += 1
-    #         # 下个状态 奖励 是否完成
-    #         len_can = np.argmax(action) + prog_args.msg_smallest_num  # 得到 下一块 数据的长度
-    #         next_state, reward, done, trained_model = graph_task.benchmark_task_val(prog_args.feat, pred_hidden_dims, len_can, log_out_file)
-    #         # 累加 奖励
-    #         ep_r += reward
-    #         # 更新 状态
-    #         state = next_state
-    #
-    #         # 数据读取完毕
-    #         # if done:
-    #         #     agent.writer.add_scalar('ep_r', ep_r, global_step=i)
-    #         #     if i % args_RL.print_log == 0:
-    #         #         print("Ep_i \t{}, the ep_r is \t{:0.2f}, the step is \t{}".format(i, ep_r, t))
-    #         #     ep_r = 0
-    #         #     break
-    #
-    # elif prog_args.mode == 'train':
-    #     print("====================================")
-    #     print("Collection Experience...")
-    #     print("====================================")
-    #
-    #     # 若指定 载入模型参数 则 载入
-    #     if prog_args.load: agent.load()
-    #
-    #
-    #     for i in range(prog_args.train_epoch):  # epoch
-    #         # 随机获取 初始状态
-    #         state, _ , _ = graph_task.benchmark_task_val(prog_args.feat, pred_hidden_dims, graph_len_, log_out_file)
-    #         # 记录 图模型 执行 步数
-    #         graph_step = 0
-    #         # 记录正确预测的 报文 个数
-    #         pred_correct = 0
-    #         # print('### main.py state.shape ###', state.shape)
-    #         # for t in range(1):
-    #         while True:
-    #
-    #             # 强化学习网络
-    #             print("====================================" * 3)
-    #             action = agent.select_action(state)  # 从 现在的 状态 得到一个动作 报文长度可选择数量
-    #             # action = action + np.random.normal(0.2, args_RL.exploration_noise, size=action.shape[0])  # 给强化学习的输出加入噪声
-    #             # action = action.clip(env.action_space.low, env.action_space.high)
-    #             # print('### main.py action.shape ###', action.shape)
-    #             # print(f'action: {action}')
-    #             # print(action)
-    #
-    #             # 图操作 步数 自增 1
-    #             graph_step += 1
-    #             # 下个状态 奖励 是否完成
-    #             len_can = np.argmax(action) + prog_args.msg_smallest_num  # 得到 下一块 数据的长度
-    #             next_state, reward, done = graph_task.benchmark_task_val(prog_args.feat, pred_hidden_dims, len_can, log_out_file)
-    #
-    #             # 数据读取完毕 跳出本轮
-    #             if done:
-    #                 agent.writer.add_scalar('ep_r', ep_r, global_step=i)
-    #                 # if i % args_RL.print_log == 0:
-    #                 #     print("Ep_i \t{}, the ep_r is \t{:0.2f}, the step is \t{}".format(i, ep_r, t))
-    #                 ep_r = 0
-    #                 break
-    #
-    #             # 累加 奖励
-    #             ep_r += reward
-    #             if reward > 0:
-    #                 pred_correct += 1
-    #             with open(log_out_file, 'a') as f:
-    #                 f.write("====================================\n")
-    #                 f.write(f'epoch: {i}; graph_step: {graph_step}; len_can: {len_can}; reward: {reward}; ep_r: {ep_r}; acc: {pred_correct/graph_step:.4f}\n')
-    #
-    #             print(f'epoch: {i}; graph_step: {graph_step}; len_can: {len_can}; reward: {reward}; ep_r: {ep_r}; acc: {pred_correct/graph_step:.4f}\n')
-    #
-    #             # 存入 经验
-    #             agent.memory.push((state.cpu().data.numpy().flatten(), next_state.cpu().data.numpy().flatten(), action, reward, np.float(done)))
-    # #             if i+1 % 10 == 0:
-    # #                 print('Episode {},  The memory size is {} '.format(i, len(agent.memory.storage)))
-    #             if len(agent.memory.storage) >= prog_args.capacity-1:
-    #                 agent.update(10, log_out_file)  # 使用经验回放 更新网络
-    #
-    #             # 更新 状态
-    #             state = next_state
-    #
-    #
-    #
-    #             # # 保存 模型
-    #             # if graph_step % args_RL.log_interval == 0:
-    #             #     agent.save()
-    #             #     break
-    #         # 保存 模型
-    #         agent.save(log_out_dir)
-    #         time_mark = time.strftime("%Y%m%d_%H%M%S", time.localtime())
-    #         with open(log_out_file, 'a') as f:
-    #             f.write(time_mark)
-    #             f.write('END')
-    #
-    # else:
-    #     raise NameError("mode wrong!!!")
+    if prog_args.mode == 'test':
+
+        # 载入 模型
+        agent.load()
+        # 记录 图模型 执行 步数
+        graph_step = 0
+        # for i in range(prog_args.train_epoch):
+        # 随机获取 初始状态
+        state, _, _ = graph_task.benchmark_task_val(prog_args.feat, pred_hidden_dims, graph_len_, log_out_file)
+
+        while True:
+            action = agent.select_action(state)  # 从 现在的 状态 得到一个动作 维度是 报文长度可选择数量
+            # 图操作 步数 自增 1
+            graph_step += 1
+            # 下个状态 奖励 是否完成
+            len_can = np.argmax(action) + prog_args.msg_smallest_num  # 得到 下一块 数据的长度
+            next_state, reward, done = graph_task.benchmark_task_val(prog_args.feat, pred_hidden_dims, len_can, log_out_file)
+            # 累加 奖励
+            ep_r += reward
+            # 更新 状态
+            state = next_state
+
+            # 数据读取完毕
+            # if done:
+            #     agent.writer.add_scalar('ep_r', ep_r, global_step=i)
+            #     if i % args_RL.print_log == 0:
+            #         print("Ep_i \t{}, the ep_r is \t{:0.2f}, the step is \t{}".format(i, ep_r, t))
+            #     ep_r = 0
+            #     break
+
+    elif prog_args.mode == 'train':
+        print("====================================")
+        print("Collection Experience...")
+        print("====================================")
+
+        # 若指定 载入模型参数 则 载入
+        if prog_args.load: agent.load()
+
+
+        for i in range(prog_args.train_epoch):  # epoch
+            # 随机获取 初始状态
+            state, _ , _ = graph_task.benchmark_task_val(prog_args.feat, pred_hidden_dims, graph_len_, log_out_file)
+            # 记录 图模型 执行 步数
+            graph_step = 0
+            # 记录正确预测的 报文 个数
+            pred_correct = 0
+            # print('### main.py state.shape ###', state.shape)
+            # for t in range(1):
+            while True:
+
+                # 强化学习网络
+                print("====================================" * 3)
+                action = agent.select_action(state)  # 从 现在的 状态 得到一个动作 报文长度可选择数量
+                # action = action + np.random.normal(0.2, args_RL.exploration_noise, size=action.shape[0])  # 给强化学习的输出加入噪声
+                # action = action.clip(env.action_space.low, env.action_space.high)
+                # print('### main.py action.shape ###', action.shape)
+                # print(f'action: {action}')
+                # print(action)
+
+                # 图操作 步数 自增 1
+                graph_step += 1
+                # 下个状态 奖励 是否完成
+                len_can = np.argmax(action) + prog_args.msg_smallest_num  # 得到 下一块 数据的长度
+                next_state, reward, done = graph_task.benchmark_task_val(prog_args.feat, pred_hidden_dims, len_can, log_out_file)
+
+                # 数据读取完毕 跳出本轮
+                if done:
+                    agent.writer.add_scalar('ep_r', ep_r, global_step=i)
+                    # if i % args_RL.print_log == 0:
+                    #     print("Ep_i \t{}, the ep_r is \t{:0.2f}, the step is \t{}".format(i, ep_r, t))
+                    ep_r = 0
+                    break
+
+                # 累加 奖励
+                ep_r += reward
+                if reward > 0:
+                    pred_correct += 1
+                with open(log_out_file, 'a') as f:
+                    f.write("====================================\n")
+                    f.write(f'epoch: {i}; graph_step: {graph_step}; len_can: {len_can}; reward: {reward}; ep_r: {ep_r}; acc: {pred_correct/graph_step:.4f}\n')
+
+                print(f'epoch: {i}; graph_step: {graph_step}; len_can: {len_can}; reward: {reward}; ep_r: {ep_r}; acc: {pred_correct/graph_step:.4f}\n')
+
+                # 存入 经验
+                agent.memory.push((state.cpu().data.numpy().flatten(), next_state.cpu().data.numpy().flatten(), action, reward, np.float(done)))
+    #             if i+1 % 10 == 0:
+    #                 print('Episode {},  The memory size is {} '.format(i, len(agent.memory.storage)))
+                if len(agent.memory.storage) >= prog_args.capacity-1:
+                    agent.update(10, log_out_file)  # 使用经验回放 更新网络
+
+                # 更新 状态
+                state = next_state
+
+                break
+
+
+
+                # # 保存 模型
+                # if graph_step % args_RL.log_interval == 0:
+                #     agent.save()
+                #     break
+            # 保存 模型
+            agent.save(log_out_dir)
+            time_mark = time.strftime("%Y%m%d_%H%M%S", time.localtime())
+            with open(log_out_file, 'a') as f:
+                f.write(time_mark)
+                f.write('END')
+
+    else:
+        raise NameError("mode wrong!!!")
 
 if __name__ == '__main__':
     main()
