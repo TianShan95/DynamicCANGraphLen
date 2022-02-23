6b5b8272432ea95f0aebf428268bdce51049b23b
Shan Tian <ts_951117aaron@163.com>
2022-02-24 02:05:49 +0800

    debug

--- main.py
+++ main.py
@@ -164,6 +164,10 @@ def main():
                 # 更新 状态
                 state = next_state
 
+                # 短期退出 epoch 验证 程序可运行行
+                if graph_step > 20:
+                    break
+
                 # # 保存 模型
                 # if graph_step % args_RL.log_interval == 0:
                 #     agent.save()
