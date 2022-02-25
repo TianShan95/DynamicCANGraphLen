7537314f8f129ceea52d68b54c4c61e21a01ddd3
Shan Tian <ts_951117aaron@163.com>
2022-02-25 18:46:23 +0800

    debug

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import time
+import os
 
 timestr = 'Fri Feb 25 18:23:08 CST 2022'
 
@@ -10,6 +11,10 @@ time1 = time.strptime(timestr, '%a %b %d %H:%M:%S CST %Y')
 # print(f'time1: {time1}')
 # print(f'localtime: {time.localtime()}')
 # print(abs(time.time() - time.mktime(time1)))
-
-if abs(time.mktime(time.strptime(timestr, '%a %b %d %H:%M:%S CST %Y')) - time.time()) < 120:
+sys_cst_time = os.popen('date').read()
+print(str(sys_cst_time))
+print('##')
+if abs(time.mktime(time.strptime(str(sys_cst_time), '%a %b %d %H:%M:%S CST %Y')) - time.time()) < 120:
     print('ok')
+else:
+    print(abs(time.mktime(time.strptime(timestr, '%a %b %d %H:%M:%S CST %Y')) - time.time()))
