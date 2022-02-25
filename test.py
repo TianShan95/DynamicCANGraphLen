c34a805f08e5850935600a6f6b02c343859825b9
Shan Tian <ts_951117aaron@163.com>
2022-02-25 18:44:02 +0800

    debug

--- test.py
+++ test.py
@@ -7,6 +7,9 @@ time1 = time.strptime(timestr, '%a %b %d %H:%M:%S CST %Y')
 # print(time.mktime(time1))
 # print(time.time())
 # timestr2 = time.strftime('%Y%m%d_%H%M%S', time.strptime(timestr, '%a %b %d %H:%M:%S CST %Y'))
-print(f'time1: {time1}')
-print(f'localtime: {time.localtime()}')
-print(time.time() - time.mktime(time1))
+# print(f'time1: {time1}')
+# print(f'localtime: {time.localtime()}')
+# print(abs(time.time() - time.mktime(time1)))
+
+if abs(time.mktime(time.strptime(timestr, '%a %b %d %H:%M:%S CST %Y')) - time.time()) < 120:
+    print('ok')
