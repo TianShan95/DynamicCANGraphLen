990811dad1ee5e2ba6afb7383ec71594f3128195
Shan Tian <ts_951117aaron@163.com>
2022-04-02 15:38:17 +0800

    debug

--- main.py
+++ main.py
@@ -32,7 +32,8 @@ setup_seed(prog_args.seed)
 
 # 状态维度
 # 这里的状态表示向量分别是 第一次图卷积操作（做了三次卷积 每次卷积产生 20 维向量） 图塌缩后 第二次卷积 同样是 产生 3 * 20 维
-state_dim = ((prog_args.num_gc_layers - 1) * prog_args.hidden_dim + prog_args.output_dim) * 2  # 60 * 2
+# state_dim = ((prog_args.num_gc_layers - 1) * prog_args.hidden_dim + prog_args.output_dim) * 2  # 60 * 2
+state_dim = 60
 # 动作维度
 action_dim = prog_args.msg_biggest_num - prog_args.msg_smallest_num  # 每个图可选取报文长度的范围
 
