c68b2257a5cf445701f3e008705c361ad7551c08
Shan Tian <ts_951117aaron@163.com>
2022-04-08 14:09:30 +0800

    debug

--- graphModel/graph.py
+++ graphModel/graph.py
@@ -161,8 +161,9 @@ def fourier(L, algo='eigh', k=1, norm = 2):
         try:
             lamb, U = np.linalg.eigh(L.toarray())
         except np.linalg.LinAlgError:
-            logger.info(f'np.linalg.LinAlgError')
-            logger.info({L})
+            logger.info(f'np.linalg.LinAlgError1')
+            logger.info({L.toarray()})
+            logger.info(f'np.linalg.LinAlgError2')
             lamb, U = scipy.linalg.eigh(L.toarray())
 
     elif algo is 'eigs':
